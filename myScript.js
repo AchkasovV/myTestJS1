a5ddffc8a2f71e24ea75b0175cc7fd2e8e7ce6d1
Vyatcheslav <arkticheskiyybelmed@rambler.ru>
2019-01-17 11:08:46 +0500

    Мой первый коммит

---
+++ myScript.js
@@ -0,0 +1,13 @@
+'use strict';
+    
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
